38879173605bdb264bc6e3cbcd8092aa192760f7
DanielBeltrami03-dev <daniel.beltrami0@gmail.com>
2025-09-26 09:46:44 +0200

    Update contact.js

--- contact.js
+++ contact.js
@@ -14,13 +14,14 @@ document.getElementById("contact-form").addEventListener("submit", function(even
 
     // Invia i dati tramite EmailJS (sostituisci con i tuoi Service ID e Template ID)
     emailjs.send("service_38giqlh", "template_yf43bfb", formObject)
-        .then(function(response) {
-            // Mostra un messaggio di successo
-            alert("Email sent successfully, you will hear from me as soon as possible");
-        }, function(error) {
-            // Mostra un messaggio di errore
-            alert("We regret to inform you that there was an issue while sending your message. Kindly reach out directly to daniel.beltrami0@gmail.com for further assistance. Thank you for your understanding and patience.");
-        });
-});
+    .then(function(response) {
+        // Mostra un messaggio di successo
+        alert("Email sent successfully, you will hear from me as soon as possible");
+    }, function(error) {
+        // Mostra un messaggio di errore
+        console.log("EmailJS Error: ", error);  // Aggiungi questo per ottenere dettagli sull'errore
+        alert("We regret to inform you that there was an issue while sending your message. Kindly reach out directly to daniel.beltrami0@gmail.com for further assistance. Thank you for your understanding and patience.");
+    });
+
 
 
