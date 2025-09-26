2688e4bb0981a6dd388a869821e1115ef0fbfe7d
DanielBeltrami03-dev <daniel.beltrami0@gmail.com>
2025-09-26 08:42:16 +0200

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,24 @@
+// Inizializza EmailJS con il tuo User ID
+emailjs.init("dTX6NgDDzST_fqrGT");
+
+// Gestore dell'evento di invio del modulo
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impedisce l'invio predefinito del modulo
+
+    // Crea un oggetto FormData per raccogliere i dati dal modulo
+    const formData = new FormData(this);
+    const formObject = {};
+    formData.forEach((value, key) => {
+        formObject[key] = value; // Associa nome, email, messaggio ai rispettivi campi
+    });
+
+    // Invia i dati tramite EmailJS (sostituisci con i tuoi Service ID e Template ID)
+    emailjs.send("service_38giqlh", "template_yf43bfb", formObject)
+        .then(function(response) {
+            // Mostra un messaggio di successo
+            alert("Email sent successfully, you will hear from me as soon as possible");
+        }, function(error) {
+            // Mostra un messaggio di errore
+            alert("We regret to inform you that there was an issue while sending your message. Kindly reach out directly to danie.beltrami0@gmail.com for further assistance. Thank you for your understanding and patience.");
+        });
+});
